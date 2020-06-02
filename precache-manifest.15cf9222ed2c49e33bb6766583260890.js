a445bef96f4e5a3f4c9d147b8cb46267fc38894c
Syed Zeeshan Haider <kazmi9266@github.com>
2020-06-02 18:11:13 +0500

    Updates

---
+++ precache-manifest.15cf9222ed2c49e33bb6766583260890.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a1c569c1983e412f22c640eeebce971",
+    "url": "/kazmi9266/my-app/index.html"
+  },
+  {
+    "revision": "3257c38d59f0de19e35e",
+    "url": "/kazmi9266/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad075e68631f83e25fbf",
+    "url": "/kazmi9266/my-app/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kazmi9266/my-app/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3257c38d59f0de19e35e",
+    "url": "/kazmi9266/my-app/static/js/main.5bc3f5dd.chunk.js"
+  },
+  {
+    "revision": "3b74b5efaee1fd7e4112",
+    "url": "/kazmi9266/my-app/static/js/runtime-main.e398fef9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kazmi9266/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
